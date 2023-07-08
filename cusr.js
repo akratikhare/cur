07bb7b83d2456455a3df968bd05bdec5da2d32b4
akratikhare <135426734+akratikhare@users.noreply.github.com>
2023-07-07 17:35:22 -0700

    first commit

---
+++ cusr.js
@@ -0,0 +1,19 @@
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cusr");
+var h1 = document.querySelector("h1");
+
+
+main.addEventListener("mousemove", function(dets){
+  
+    cursor.style.left = dets.x + "px"
+    cursor.style.top = dets.y + "px"
+
+})
+h1.addEventListener("mouseenter", function(){
+    cursor.style.scale = 2.5
+})
+h1.addEventListener("mouseleave" , function(){
+    cursor.style.scale = 1 
+})
+
+
